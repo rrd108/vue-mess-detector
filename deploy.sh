a9ae219a6c78d09d62b0a0b9dee5b52d92de91a9
rrd108 <rrd@webmania.cc>
2024-06-03 19:15:01 +0200

    chore release notes added to deploy script

--- deploy.sh
+++ deploy.sh
@@ -37,6 +37,14 @@ if [ -z "$git_status" ]; then
 
   npx jsr publish
 
+# Collect release notes from commits since the last release
+  last_release=$(git describe --tags --abbrev=0)
+  release_notes=$(git log "${last_release}..HEAD" --pretty="%s" | awk -v prefix="* " '/^(feat|fix|perf|docs|test|chore|refactor|style|build|ci|revert)(\([a-z]+\))?(!\?)?:/{print prefix $0}')
+
+  # Push the new tag and create a release
+  git push origin "v$new_version"
+  gh release create "v$new_version" --notes "Release version $new_version"
+
 else
   echo "Repository is not clean. Please commit or stash your changes before publishing."
   exit 1
