200d70208cddc21767e4804c92f853340bc2a27d
rrd108 <rrd@webmania.cc>
2024-06-05 11:59:16 +0200

    chore added emoji to deploy output

--- deploy.sh
+++ deploy.sh
@@ -46,26 +46,31 @@ if [ $PREV_STEP -eq 1 ];then
 
     # Perform publishing, pushing, and release creation only if the dry-run flag is not set
     if [ "$2" == "publish" ]; then
+      echo "👉 Publishing the new version to npmjs.com"
       yarn publish --new-version "$1"
 
       # Capture the new version number
       new_version=$(node -p "require('./package.json').version")
       
+      echo "👉 Pushing new version to git: $new_version"
       git push vue-mess-detector "$new_version"
+
+      echo "👉 Creating a new release on GitHub"
       gh release create "$new_version" --notes "$release_notes"
 
       # Update the version field in jsr.json
       jq ".version = \"$new_version\"" jsr.json > tmp.json && mv tmp.json jsr.json
 
-      echo "jsr.json updated with the new version: $new_version"
+      echo "👉 jsr.json updated with the new version: $new_version"
 
       # Commit the changes to Git
       git add jsr.json
       git commit -m "Bump version to $new_version"
 
-      echo "Changes committed to Git"
+      echo "👉 Changes committed to Git"
 
       npx jsr publish
+      echo "👉 Published the new version to JSR"
     else
       echo "Dry run mode. No publishing or pushing will be performed."
     fi
