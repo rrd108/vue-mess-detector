6dccd030bbd1ac6aa8759de2e741cfdef93af81c
David-Pena <dev.davidpe@gmail.com>
2024-08-06 23:05:04 -0700

    feat: add vue-caution element-selectors-with-scoped rule w tests

---
+++ src/rules/vue-caution/elementSelectorsWithScoped.ts
@@ -0,0 +1,50 @@
+import type { SFCStyleBlock } from '@vue/compiler-sfc'
+import { BG_RESET, BG_WARN, TEXT_INFO, TEXT_RESET, TEXT_WARN } from '../asceeCodes'
+import { getUniqueFilenameCount } from '../../helpers'
+
+interface ElementSelectorsWithScoped {
+  filename: string
+  selector: string
+}
+
+const validHtmlTags = ['div', 'span', 'button', 'input', 'form', 'label']
+
+const elementSelectorsWithScopedFiles: ElementSelectorsWithScoped[] = []
+
+const checkElementSelectorsWithScoped = (styles: SFCStyleBlock[] | null, filePath: string) => {
+  if (!styles)
+    return
+
+  const elementSelectorRegex = /([a-z]+)\s*\{[^}]*\}/gi
+
+  styles.forEach((style) => {
+    let match
+    // eslint-disable-next-line no-cond-assign
+    while ((match = elementSelectorRegex.exec(style.content)) !== null) {
+      const selector = match[1]
+      if (validHtmlTags.includes(selector)) {
+        elementSelectorsWithScopedFiles.push({ filename: filePath, selector })
+      }
+    }
+  })
+}
+
+const reportElementSelectorsWithScoped = () => {
+  if (elementSelectorsWithScopedFiles.length > 0) {
+    // Count only non duplicated objects (by its `filename` property)
+    const fileCount = getUniqueFilenameCount<ElementSelectorsWithScoped>(elementSelectorsWithScopedFiles, 'filename')
+
+    console.log(
+      `\n${TEXT_INFO}vue-caution${TEXT_RESET} ${BG_WARN}element selectors with scoped${BG_RESET} found in ${fileCount} files.`,
+    )
+    console.log(
+      `👉 ${TEXT_WARN}Prefer class selectors over element selectors in scoped styles, because large numbers of element selectors are slow.${TEXT_RESET} See: https://vuejs.org/style-guide/rules-use-with-caution.html#element-selectors-with-scoped`,
+    )
+    elementSelectorsWithScopedFiles.forEach((file) => {
+      console.log(` - ${file.filename} 🚨 ${BG_WARN}(${file.selector})${BG_RESET}`)
+    })
+  }
+  return elementSelectorsWithScopedFiles.length
+}
+
+export { checkElementSelectorsWithScoped, reportElementSelectorsWithScoped }
