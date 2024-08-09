ddb815d130cad87a84fae4d0ec4b9676c95731b3
rrd108 <rrd@webmania.cc>
2024-08-09 18:03:22 +0200

    refactor for #120

--- src/rules/vue-essential/vifWithVfor.test.ts
+++ src/rules/vue-essential/vifWithVfor.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import type { SFCTemplateBlock } from '@vue/compiler-sfc'
-import { TEXT_INFO, TEXT_RESET, TEXT_WARN } from '../asceeCodes'
+import { BG_RESET, BG_WARN, TEXT_INFO, TEXT_RESET, TEXT_WARN } from '../asceeCodes'
 import { checkVifWithVfor, reportVifWithVfor, resetVIfWithVFor } from './vifWithVfor'
 
 describe('checkVifWithVfor', () => {
@@ -47,7 +47,7 @@ describe('checkVifWithVfor', () => {
       file: fileName,
       rule: `${TEXT_INFO}vue-essential ~ v-if used with v-for${TEXT_RESET}`,
       description: `👉 ${TEXT_WARN}Move out the v-if to a computed property.${TEXT_RESET} See: https://vuejs.org/style-guide/rules-essential.html#avoid-v-if-with-v-for`,
-      message: `🚨`,
+      message: `${BG_WARN}v-if used with v-for${BG_RESET} 🚨`,
     }])
   })
 })
