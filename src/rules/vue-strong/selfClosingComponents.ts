0fcf0a6c08274fca187f9f38dc64408d67c8997c
rrd108 <rrd@webmania.cc>
2024-08-09 18:19:58 +0200

    refactor for #120

--- src/rules/vue-strong/selfClosingComponents.ts
+++ src/rules/vue-strong/selfClosingComponents.ts
@@ -2,9 +2,9 @@ import type { SFCDescriptor } from '@vue/compiler-sfc'
 import { charNotIn, createRegExp, letter, linefeed, maybe, oneOrMore, tab, wordChar } from 'magic-regexp'
 import { BG_RESET, BG_WARN, TEXT_INFO, TEXT_RESET, TEXT_WARN } from '../asceeCodes'
 import getLineNumber from '../getLineNumber'
-import type { Offense } from '../../types'
+import type { FileCheckResult, Offense } from '../../types'
 
-const selfClosingComponentsFiles: { filename: string, message: string }[] = []
+const results: FileCheckResult[] = []
 
 const checkSelfClosingComponents = (descriptor: SFCDescriptor | null, filePath: string) => {
   if (!descriptor) {
@@ -30,26 +30,26 @@ const checkSelfClosingComponents = (descriptor: SFCDescriptor | null, filePath: 
   matches?.forEach((componentTag) => {
     const lineNumber = getLineNumber(descriptor.source, componentTag)
     const lastPart = componentTag.split('\n').at(-1)?.trim() || ''
-    selfClosingComponentsFiles.push({ filename: filePath, message: `line #${lineNumber} ${BG_WARN}${lastPart}${BG_RESET}` })
+    results.push({ filePath, message: `line #${lineNumber} ${BG_WARN}${lastPart}${BG_RESET}` })
   })
 }
 
 const reportSelfClosingComponents = () => {
   const offenses: Offense[] = []
 
-  if (selfClosingComponentsFiles.length > 0) {
-    selfClosingComponentsFiles.forEach((file) => {
+  if (results.length > 0) {
+    results.forEach((result) => {
       offenses.push({
-        file: file.filename,
+        file: result.filePath,
         rule: `${TEXT_INFO}vue-strong ~ component is not self closing${TEXT_RESET}`,
         description: `👉 ${TEXT_WARN}Components with no content should be self-closing.${TEXT_RESET} See: https://vuejs.org/style-guide/rules-strongly-recommended.html#self-closing-components`,
-        message: `${file.message} 🚨`,
+        message: `${result.message} 🚨`,
       })
     })
   }
   return offenses
 }
 
-const resetSelfClosingComponents = () => (selfClosingComponentsFiles.length = 0)
+const resetSelfClosingComponents = () => (results.length = 0)
 
 export { checkSelfClosingComponents, reportSelfClosingComponents, resetSelfClosingComponents }
