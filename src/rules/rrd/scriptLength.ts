2cf7c62b5dfa43dd1c06f402fac0edba60fa96a7
rrd108 <rrd@webmania.cc>
2024-08-09 18:36:32 +0200

    refactor for #120

--- src/rules/rrd/scriptLength.ts
+++ src/rules/rrd/scriptLength.ts
@@ -1,39 +1,40 @@
 import type { SFCScriptBlock } from '@vue/compiler-sfc'
 import { BG_ERR, BG_RESET, BG_WARN, TEXT_INFO, TEXT_RESET, TEXT_WARN } from '../asceeCodes'
-import type { Offense } from '../../types'
+import type { FileCheckResult, Offense } from '../../types'
+
+const results: FileCheckResult[] = []
 
 export const MAX_SCRIPT_LENGTH = 100
 
-const longScriptFiles: { fileName: string, scriptLength: number }[] = []
-
-const checkScriptLength = (script: SFCScriptBlock | null, file: string) => {
+const checkScriptLength = (script: SFCScriptBlock | null, filePath: string) => {
   if (!script) {
     return
   }
   const lines = script.content.split('\n')
   if (lines.length > MAX_SCRIPT_LENGTH) {
-    longScriptFiles.push({ fileName: file, scriptLength: lines.length })
+    results.push({ filePath, message: `${lines.length > MAX_SCRIPT_LENGTH * 2 ? BG_ERR : BG_WARN}(${
+      lines.length
+    } lines)${BG_RESET}` })
   }
 }
 
 const reportScriptLength = () => {
   const offenses: Offense[] = []
 
-  if (longScriptFiles.length > 0) {
-    longScriptFiles.forEach((file) => {
+  if (results.length > 0) {
+    results.forEach((result) => {
       offenses.push({
-        file: file.fileName,
+        file: result.filePath,
         rule: `${TEXT_INFO}rrd ~ Long <script> blocks${TEXT_RESET}`,
         description: `👉 ${TEXT_WARN}Try to refactor out the logic into composable functions or other files and keep the script block's length under ${MAX_SCRIPT_LENGTH} lines.${TEXT_RESET}`,
-        message: `${file.scriptLength > MAX_SCRIPT_LENGTH * 2 ? BG_ERR : BG_WARN}(${
-          file.scriptLength
-        } lines)${BG_RESET} 🚨`,
+        message: `${result.message} 🚨`,
+
       })
     })
   }
   return offenses
 }
 
-const resetScriptLength = () => (longScriptFiles.length = 0)
+const resetScriptLength = () => (results.length = 0)
 
 export { checkScriptLength, reportScriptLength, resetScriptLength }
