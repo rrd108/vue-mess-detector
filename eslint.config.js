522117015a3c39b8af1824a1dac087b0c15bb9e9
rrd108 <rrd@webmania.cc>
2024-08-14 14:35:41 +0200

    disable eslint for markdown fix #132

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({}, {
+  markdown: {
+    codeBlocks: {
+      vue: false // Disable Vue code block processing in Markdown
+    }
+  },
   rules: {
     // allow console and debugger
     'no-console': 'off',
