1ac78e0f6ad1df90de468312445e2bd55f66ffc2
rrd108 <rrd@webmania.cc>
2024-08-22 17:02:49 +0200

    chore fix vscode and eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,21 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({}, {
-  markdown: {
-    codeBlocks: {
-      vue: false // Disable Vue code block processing in Markdown
-    }
+export default antfu(
+  {
+    markdown: {
+      codeBlocks: {
+        vue: false, // Disable Vue code block processing in Markdown
+      },
+    },
   },
-  rules: {
-    // allow console and debugger
-    'no-console': 'off',
-    'no-debugger': 'off',
-    // ignore top-level functions should be declared with function keywordes
-    'antfu/top-level-function': 'off',
+  {
+    rules: {
+      // allow console and debugger
+      'no-console': 'off',
+      'no-debugger': 'off',
+      // ignore top-level functions should be declared with function keywordes
+      'antfu/top-level-function': 'off',
+      'style/semi': ['error', 'never'],
+    },
   },
-})
+)
