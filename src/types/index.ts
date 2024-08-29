ecfa646ea984e58bb0fbbac4dc20b335a868cde8
rrd108 <rrd@webmania.cc>
2024-08-29 16:34:03 +0200

    chore todo comment added

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ export type Flag = 'groupBy' | 'orderBy' | 'outputLevel' | 'outputFormat'
 export type GroupBy = 'rule' | 'file'
 export type OrderBy = 'asc' | 'desc'
 export type OutputLevel = 'all' | 'error'
+// TODO refactor the above to be like ouput formats - validateOption and src/index.ts should be also changed
 export const OUTPUT_FORMATS = ['text', 'json', 'table'] as const
 export type OutputFormat = typeof OUTPUT_FORMATS[number]
 
