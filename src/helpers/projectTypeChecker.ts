5b6091b928df0c854cdecfe1175d8303e0a893cd
rrd108 <rrd@webmania.cc>
2024-09-13 17:43:02 +0200

    refactor replace top level await

--- src/helpers/projectTypeChecker.ts
+++ src/helpers/projectTypeChecker.ts
@@ -3,10 +3,13 @@ import path from 'node:path'
 import { getPackageJson } from './getPackageJson'
 import getProjectRoot from './getProjectRoot'
 
-// eslint-disable-next-line node/prefer-global/process
-const projectPath = await getProjectRoot(process.cwd()) || ''
+const getProjectPath = async () => {
+  // eslint-disable-next-line node/prefer-global/process
+  return await getProjectRoot(process?.cwd() || './') || ''
+}
 
 const checkDependency = async (dependencyName: 'vue' | 'nuxt', projectRoot: string) => {
+  const projectPath = await getProjectPath()
   const packageJsonPath = path.join(projectPath, 'package.json')
   if (fs.existsSync(packageJsonPath)) {
     const packageJson = await getPackageJson(projectRoot)
@@ -16,11 +19,13 @@ const checkDependency = async (dependencyName: 'vue' | 'nuxt', projectRoot: stri
 }
 
 const isNuxtProject = async (projectRoot: string) => {
+  const projectPath = await getProjectPath()
   const nuxtConfigFiles = ['nuxt.config.js', 'nuxt.config.ts']
   return await checkDependency('nuxt', projectRoot) || nuxtConfigFiles.some(file => fs.existsSync(path.join(projectPath, file)))
 }
 
 const isVueProject = async (projectRoot: string) => {
+  const projectPath = await getProjectPath()
   const vueConfigFiles = ['vue.config.js', 'vue.config.ts']
   const isNuxt = await isNuxtProject(projectRoot)
   return !isNuxt && (await checkDependency('vue', projectRoot) || vueConfigFiles.some(file => fs.existsSync(path.join(projectPath, file))))
