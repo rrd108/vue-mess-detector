1e8133bed69eb543e31b6723ea794ba7b26511a4
David-Pena <dev.davidpe@gmail.com>
2024-09-20 22:10:56 -0700

    fix: improve nestedTernary checking method to handle edge-cases (#298)

--- src/rules/rrd/nestedTernary.test.ts
+++ src/rules/rrd/nestedTernary.test.ts
@@ -36,7 +36,7 @@ describe('checkNestedTernary', () => {
     }])
   })
 
-  it.todo('should not report files when optional chaning is used along with a ternary', () => {
+  it('should not report files when optional chaning is used along with a ternary', () => {
     const script = {
       content: `const countDatasets = this.structure?.volume ? this.structure.volume.length : 0;`,
     } as SFCScriptBlock
@@ -45,5 +45,37 @@ describe('checkNestedTernary', () => {
     const result = reportNestedTernary()
     expect(result.length).toBe(0)
     expect(result).toStrictEqual([])
+  })
+
+  it('should not report files with simple ternary and optional chaining', () => {
+    const script = {
+      content: `
+        const result = obj?.property ? obj.property : defaultValue;
+        const anotherResult = condition ? value1 : value2;
+      `,
+    } as SFCScriptBlock
+    const fileName = 'simple-ternary-and-optional-chaining.vue'
+    checkNestedTernary(script, fileName)
+    const result = reportNestedTernary()
+    expect(result.length).toBe(0)
+    expect(result).toStrictEqual([])
+  })
+
+  it('should report files with nested ternary, even when optional chaining is present', () => {
+    const script = {
+      content: `
+        const result = obj?.property ? (condition ? value1 : value2) : defaultValue;
+      `,
+    } as SFCScriptBlock
+    const fileName = 'nested-ternary-with-optional-chaining.vue'
+    checkNestedTernary(script, fileName)
+    const result = reportNestedTernary()
+    expect(result.length).toBe(1)
+    expect(result).toStrictEqual([{
+      file: fileName,
+      rule: `<text_info>rrd ~ nested Ternary</text_info>`,
+      description: `👉 <text_warn>Break the nested ternary into standalone ternaries, if statements, && operators, or a dedicated function.</text_warn> See: https://vue-mess-detector.webmania.cc/rules/rrd/nested-ternary.html`,
+      message: `line #1 has <bg_warn>nested ternary</bg_warn> 🚨`,
+    }])
   })
 })
