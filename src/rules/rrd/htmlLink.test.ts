6fea654b86259cf851a19b8cc1c3208ea09fe1be
David-Pena <dev.davidpe@gmail.com>
2024-09-26 21:20:42 -0700

    chore: add failing test for htmlLinks for #340

--- src/rules/rrd/htmlLink.test.ts
+++ src/rules/rrd/htmlLink.test.ts
@@ -18,6 +18,23 @@ describe('checkHtmlLink', () => {
     expect(result).toStrictEqual([])
   })
 
+  it.todo('should not report files with commented HTML links', () => {
+    const template = {
+      content: `<template>
+          <SectionText>
+            The application is dependent on the following remote services.See xxxx
+            <!-- <a class="cursor-pointer text-link underline hover:no-underline dark:text-link-dark" @click="openHelpArticleInSupportWidget('33-the-device')">this help article</a> -->
+            for more technical details.
+          </SectionText>
+        </template>`,
+    } as SFCTemplateBlock
+    const fileName = 'commented-html-link.vue'
+    checkHtmlLink(template, fileName)
+    const result = reportHtmlLink()
+    expect(result.length).toBe(0)
+    expect(result).toStrictEqual([])
+  })
+
   it('should report files with HTML links', () => {
     const template = {
       content: `<template>
